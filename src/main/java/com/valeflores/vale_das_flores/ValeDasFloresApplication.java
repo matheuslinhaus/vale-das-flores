9cb44a33d741436d2dedf3c85f9262f628814db9
Matheus R Linhaus <matheuslinhauss@hotmail.com>
2025-01-06 18:59:30 -0300

    first commit

---
+++ src/main/java/com/valeflores/vale_das_flores/ValeDasFloresApplication.java
@@ -0,0 +1,13 @@
+package com.valeflores.vale_das_flores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ValeDasFloresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ValeDasFloresApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/valeflores/vale_das_flores/ValeDasFloresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.valeflores.vale_das_flores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ValeDasFloresApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
