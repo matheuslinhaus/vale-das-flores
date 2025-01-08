b6aa39b91f06fcc9e51512ada4afb45066e4322d
Matheus R Linhaus <matheuslinhauss@hotmail.com>
2025-01-08 18:28:55 -0300

    Ajustar processos em relação as exceções. Foi adicionado um processo para que seja validada cada campo preenchido.

--- src/main/java/com/valeflores/vale_das_flores/services/UserService.java
+++ src/main/java/com/valeflores/vale_das_flores/services/UserService.java
@@ -1,5 +1,8 @@
 package com.valeflores.vale_das_flores.services;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -7,7 +10,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import com.valeflores.vale_das_flores.entities.User;
 import com.valeflores.vale_das_flores.repositories.UserRepository;
-import com.valeflores.vale_das_flores.services.exceptions.EmailException;
+import com.valeflores.vale_das_flores.services.exceptions.RegisterException;
 
 @Service
 public class UserService {
@@ -16,6 +19,8 @@ public class UserService {
 	private UserRepository repository;
 
 	private PasswordEncoder passwordEncoder;
+	
+	private static final String EMAIL_PATTERN = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}$";
 
 	public UserService(PasswordEncoder passwordEncoder) {
 		this.passwordEncoder = passwordEncoder;
@@ -23,8 +28,19 @@ public class UserService {
 
 	@Transactional
 	public User insert(User user) {
+        if (!isValidName(user.getName())) {
+            throw new RegisterException("Your name must include both first and last names, with a minimum of 8 characters.");
+        }
+		
+		if (!isValidEmail(user.getEmail())) {
+			throw new RegisterException("Invalid email. Please provide a valid email address.");
+		}
 		if (repository.existsByEmail(user.getEmail())) {
-			throw new EmailException("Email already in use.");
+			throw new RegisterException("Email already in use.");
+		}
+		
+		if (!isValidPassword(user.getPassword())) {
+			throw new RegisterException("Your password must be at least 8 characters long for security reasons.");
 		}
 
 		user.setPassword(passwordEncoder.encode(user.getPassword()));
@@ -35,10 +51,24 @@ public class UserService {
 		User user = repository.findByEmail(email);
 
 		if (user == null) {
-			return false; // Se o usuário não existe, retorno false
+			return false;
 		}
 
-		// Verifica se a senha fornecida corresponde à senha criptografada no banco
 		return passwordEncoder.matches(password, user.getPassword());
 	}
+	
+	private boolean isValidEmail(String email) {
+        Pattern pattern = Pattern.compile(EMAIL_PATTERN);
+        Matcher matcher = pattern.matcher(email);
+        return matcher.matches();
+    }
+	
+	private boolean isValidName(String name) {
+		return name != null && name.length() >= 8 && name.contains(" ");
+	}
+	
+	private boolean isValidPassword(String password) {
+		return password.length() >= 8;
+	}
+	
 }
